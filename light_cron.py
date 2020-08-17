0e93b7bd6ce879927b3856726e435abad63b672d
Ian Kline <kline.ian.d@gmail.com>
2020-08-17 00:25:45 -0400

    efwefwfwef

--- light_cron.py
+++ light_cron.py
@@ -1,8 +1,8 @@
 import subprocess
 subprocess.check_output('cd /home/pi/Lights', shell=True)
-subprocess.check_output('git stash', shell=True)
+#subprocess.check_output('git stash', shell=True)
 direct_output = subprocess.check_output('git pull https://github.com/blurrydude/Lights.git', shell=True)
-subprocess.check_output('git stash clear', shell=True)
+#subprocess.check_output('git stash clear', shell=True)
 
 if direct_output!=b'Already up to date.\n':
     print('restarting')
