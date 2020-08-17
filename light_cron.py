08e2f503ebd8667da0ec9bf9dce550e134f299d7
Ian Kline <kline.ian.d@gmail.com>
2020-08-16 23:47:50 -0400

    temp disable cd and reboot

--- light_cron.py
+++ light_cron.py
@@ -1,7 +1,7 @@
 import subprocess
-subprocess.check_output('cd /home/pi/Lights', shell=True)
+#subprocess.check_output('cd /home/pi/Lights', shell=True)
 subprocess.check_output('git stash', shell=True)
 direct_output = subprocess.check_output('git pull', shell=True)
 subprocess.check_output('git stash clear', shell=True)
-subprocess.check_output('reboot', shell=True)
+#subprocess.check_output('reboot', shell=True)
 print(direct_output)
