b8ccee80fd8f7c2fc5b9ac2d5fe1a27bbd330bb9
Ian Kline <kline.ian.d@gmail.com>
2020-08-17 01:03:44 -0400

    svsvs

--- light_cron.py
+++ light_cron.py
@@ -3,7 +3,7 @@ import subprocess
 direct_output = subprocess.check_output('cd /home/pi/Lights && git pull --all', shell=True)
 #subprocess.check_output('git stash clear', shell=True)
 
-if direct_output != b'Already up to date.\n':
+if direct_output != b'Already up to date.\n' or direct_output != b'warning: redirecting to https://github.com/blurrydude/Lights.git/\nAlready up to date.\n':
     print('restarting')
     subprocess.check_output('reboot now', shell=True)
 else:
