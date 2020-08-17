730b773e3315cad724bdf7fe4b64fdad21d3777a
Ian Kline <kline.ian.d@gmail.com>
2020-08-16 23:52:42 -0400

    light cron armed

--- light_cron.py
+++ light_cron.py
@@ -1,9 +1,9 @@
 import subprocess
-#subprocess.check_output('cd /home/pi/Lights', shell=True)
+subprocess.check_output('cd /home/pi/Lights', shell=True)
 subprocess.check_output('git stash', shell=True)
 direct_output = subprocess.check_output('git pull', shell=True)
 subprocess.check_output('git stash clear', shell=True)
 
 if direct_output!=b'Already up to date.\n':
     print('restarting')
-    #subprocess.check_output('reboot', shell=True)
+    subprocess.check_output('reboot', shell=True)
