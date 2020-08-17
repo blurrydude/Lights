2793d71879316e9f93af6f063d5edb65a791f99c
Ian Kline <kline.ian.d@gmail.com>
2020-08-17 01:01:11 -0400

    wfewef

--- light_cron.py
+++ light_cron.py
@@ -1,7 +1,6 @@
 import subprocess
-subprocess.check_output('cd /home/pi/Lights', shell=True)
 #subprocess.check_output('git stash', shell=True)
-direct_output = subprocess.check_output('git pull', shell=True)
+direct_output = subprocess.check_output('cd /home/pi/Lights && git pull --all', shell=True)
 #subprocess.check_output('git stash clear', shell=True)
 
 if direct_output != b'Already up to date.\n':
