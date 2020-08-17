2108ff63d4bfafe1c31a1f42bedc4d8fdbc71818
Ian Kline <kline.ian.d@gmail.com>
2020-08-16 23:51:07 -0400

    more changes

--- light_cron.py
+++ light_cron.py
@@ -3,5 +3,7 @@ import subprocess
 subprocess.check_output('git stash', shell=True)
 direct_output = subprocess.check_output('git pull', shell=True)
 subprocess.check_output('git stash clear', shell=True)
-#subprocess.check_output('reboot', shell=True)
-print(direct_output==b'Already up to date.\n')
+
+if direct_output!=b'Already up to date.\n':
+    print('restarting')
+    #subprocess.check_output('reboot', shell=True)
