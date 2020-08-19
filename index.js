b6ff158f701eec2fdfb8c2fab6d91ce6c7e92540
Ian Kline <kline.ian.d@gmail.com>
2020-08-18 21:48:56 -0400

    update

--- index.js
+++ index.js
@@ -278,7 +278,7 @@ $.get(window.location.origin + '/mem', function(response) {
 $.get(window.location.origin + '/config', function(response) {
     console.log(response);
     config = response;
-    $('#namespan').innerHTML = config.name;
+    document.getElementById("namespan").innerHTML = config.name;
     updateConfig();
 });
 
