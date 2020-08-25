55d4e85c243ee2580d0bc27011f1d5a2a256bdda
Ian Kline <kline.ian.d@gmail.com>
2020-08-24 23:28:35 -0400

    clock work (get it?)

--- clock.py
+++ clock.py
@@ -44,7 +44,7 @@ else:
 #     setPixel(hourhand-5,0,0,0)
 # if minutehand > 0:
 #     setPixel(minutehand-1,0,0,0)
-while datetime.now().minute == minutehand:
+while tick < 56:
     if minutehand != tick and hourhand != tick:
         if tick == 0:
             setPixel(0,0,4,32)
