78c5ef330187f823c37375ffd32c6eee19992644
Ian Kline <kline.ian.d@gmail.com>
2020-08-24 23:57:14 -0400

    updates

--- clock.py
+++ clock.py
@@ -7,6 +7,8 @@ def setPixel(p,r,g,b):
 def setRange(a,z,r,g,b):
     requests.get('http://192.168.1.236/?r='+str(r)+'&g='+str(g)+'&b='+str(b)+'&a='+str(a)+'&z='+str(z))
 
+doSeconds = False
+
 now = datetime.now()
 hour = now.hour
 postmeridian = hour > 11
@@ -46,35 +48,36 @@ else:
 #     setPixel(minutehand-1,0,0,0)
 nexttick = 0
 startsecond = second
-while datetime.now().minute == minutehand or datetime.now().second < startsecond -2:
-    if minutehand != tick and hourhand != tick and tick == nexttick:
-        if tick == 0:
-            setPixel(0,0,4,32)
-        elif tick == 5:
-            setPixel(5,0,4,32)
-        elif tick == 10:
-            setPixel(10,0,4,32)
-        elif tick == 15:
-            setPixel(15,0,4,32)
-        elif tick == 20:
-            setPixel(20,0,4,32)
-        elif tick == 25:
-            setPixel(25,0,4,32)
-        elif tick == 30:
-            setPixel(30,0,4,32)
-        elif tick == 35:
-            setPixel(35,0,4,32)
-        elif tick == 40:
-            setPixel(40,0,4,32)
-        elif tick == 45:
-            setPixel(45,0,4,32)
-        elif tick == 50:
-            setPixel(50,0,4,32)
-        elif tick == 55:
-            setPixel(55,0,4,32)
-        # else:
-        #     setPixel(tick,8,0,16)
-        nexttick = tick + 1
-    if second != datetime.now().second:
-        second = datetime.now().second
-        tick = tick + 1
+if doSeconds == True:
+    while datetime.now().minute == minutehand or datetime.now().second < startsecond -2:
+        if minutehand != tick and hourhand != tick and tick == nexttick:
+            if tick == 0:
+                setPixel(0,0,4,32)
+            elif tick == 5:
+                setPixel(5,0,4,32)
+            elif tick == 10:
+                setPixel(10,0,4,32)
+            elif tick == 15:
+                setPixel(15,0,4,32)
+            elif tick == 20:
+                setPixel(20,0,4,32)
+            elif tick == 25:
+                setPixel(25,0,4,32)
+            elif tick == 30:
+                setPixel(30,0,4,32)
+            elif tick == 35:
+                setPixel(35,0,4,32)
+            elif tick == 40:
+                setPixel(40,0,4,32)
+            elif tick == 45:
+                setPixel(45,0,4,32)
+            elif tick == 50:
+                setPixel(50,0,4,32)
+            elif tick == 55:
+                setPixel(55,0,4,32)
+            # else:
+            #     setPixel(tick,8,0,16)
+            nexttick = tick + 1
+        if second != datetime.now().second:
+            second = datetime.now().second
+            tick = tick + 1
