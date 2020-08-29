90ce61e54c61f41fc638f3a3208b40ab8ec4ecb0
Ian Kline <kline.ian.d@gmail.com>
2020-08-28 23:31:31 -0400

    motor_server.py

--- motor_server.py
+++ motor_server.py
@@ -13,6 +13,7 @@ import json
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 import threading
+import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--ip", "-ip", help="ip address")
@@ -41,6 +42,36 @@ def stepperDaemon():
             time.sleep(1/ss)
         else:
             time.sleep(0.01)
+
+def checkCommandDaemon():
+    global settings
+    command_topic = requests.get('https://blurrydude.com/topic?t=commands').text()
+    if "motor" in command_topic:
+        requests.get('https://blurrydude.com/ack?t=commands')
+        if "dc" in command_topic:
+            if "run" in command_topic:
+                if "1" in command_topic or "one" in command_topic:
+                    settings["dcmotor1"] = 1
+                    setMotorSpeed(1,1)
+                elif "2" in command_topic or "two" in command_topic:
+                    settings["dcmotor2"] = 1
+                    setMotorSpeed(2,1)
+            elif "stop" in command_topic:
+                if "1" in command_topic or "one" in command_topic:
+                    settings["dcmotor1"] = 1
+                    setMotorSpeed(1,0)
+                elif "2" in command_topic or "two" in command_topic:
+                    settings["dcmotor2"] = 1
+                    setMotorSpeed(2,0)
+        elif "stepper" in command_topic:
+            if "run" in command_topic:
+                if "forward" in command_topic:
+                    settings["stepper"] = 200
+                elif "backward" in command_topic:
+                    settings["stepper"] = -200
+            elif "stop" in command_topic:
+                settings["stepper"] = 0
+                
 
 def setMotorSpeed(motor, speed):
     global direction
