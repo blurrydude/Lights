f656550fcbb9dbb44f0e5ebb0bc7357c62071ac0
Ian Kline <kline.ian.d@gmail.com>
2020-08-31 23:13:19 -0400

    health_manager.py

--- health_manager.py
+++ health_manager.py
@@ -7,3 +7,9 @@ from datetime import datetime
 now = datetime.now()
 
 #delete old logs here
+mydir = "/home/pi/logs/"
+filelist = [ f for f in os.listdir(mydir) if f.endswith(".bak") ]
+for f in filelist:
+    if datetime.now().strftime("%Y-%m-%d") in f:
+        continue
+    os.remove(os.path.join(mydir, f))
