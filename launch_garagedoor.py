9ff5a535023be4bad50fb89f10db2fb3be881292
Ian Kline <kline.ian.d@gmail.com>
2020-08-31 23:43:52 -0400

    launch_garagedoor.py

--- launch_garagedoor.py
+++ launch_garagedoor.py
@@ -7,5 +7,5 @@ parser.add_argument("--ip", "-ip", help="ip address")
 parser.add_argument("--s", "-s", help="whether or not to use sensor input (0 or 1)")
 args = parser.parse_args()
 
-time.sleep(30)
+time.sleep(90)
 os.system('sudo python3 /home/pi/Lights/garagedoor.py -ip '+args.ip+' -s '+args.s)
