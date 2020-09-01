fa109a29bb2a4743d2ff4e8dd12210867ceac7c6
Ian Kline <kline.ian.d@gmail.com>
2020-08-31 20:22:23 -0400

    garagedoor.py

--- garagedoor.py
+++ garagedoor.py
@@ -1,6 +1,21 @@
 import pifacedigitalio as p
 import time
 p.init()
-p.digital_write(0,1)
-time.sleep(10)
-p.digital_write(0,0)
+
+def waitFor(i):
+    while p.digital_read(i) == 0: # FLIP THIS WHEN REED SWITCH IS INSTALLED
+        time.sleep(0.1)
+
+def openDoor():
+    p.digital_write(0,1)
+    time.sleep(0.5)
+    p.digital_write(0,0)
+
+def closeDoor():
+    p.digital_write(0,1)
+    waitFor(1)
+    p.digital_write(0,0)
+
+openDoor()
+time.sleep(2)
+closeDoor()
