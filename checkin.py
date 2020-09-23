4f2881a35a5e0d24ccdf4ec3dc7484be12a65fc5
Ian Kline <kline.ian.d@gmail.com>
2020-09-22 23:15:16 -0400

    pi4in1 is a jerk

--- checkin.py
+++ checkin.py
@@ -3,10 +3,16 @@ import socket
 import os
 import json
 from crontab import CronTab
+import urllib
 
 URL = "https://blurrydude.com:5000/checkin"
 
 name = socket.gethostname()
+if name == 'pi4in1':
+    check_online = urllib.request.urlopen("https://www.google.com").getcode()
+    if check_online != 200:
+        os.system('sudo reboot now')
+        exit()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
@@ -56,74 +62,3 @@ if name.replace('pi','') in command:
         os.system('reboot now')
     else:
         requests.get('http:'+ip+'/command?c='+command)
-#  MMFFFFFFFFFLLLSSDDVV
-# M - model
-# F - device flags
-# L - led count
-# S - stepper motor count
-# D - dc motor count
-# V - servo moto count
-
-# var PiModel = {
-#     M1A: 0,
-#     M1AP: 1,
-#     M1B: 2,
-#     M1BP: 3,
-#     M2B: 4,
-#     M2BV12: 5,
-#     M3AP: 6,
-#     M3B: 7,
-#     M3BP: 8,
-#     M4B: 9,
-#     CM1: 10,
-#     CM3: 11,
-#     CM3L: 12,
-#     CM3P: 13,
-#     CM3LP: 14,
-#     ZV12: 15,
-#     ZV13: 16,
-#     ZW: 17
-# }
-
-# var DeviceFlags = {
-#     HEADLESS: 1,
-#     LED_CONTROLLER: 2,
-#     FILE_SERVER: 4,
-#     WEB_SERVER: 8,
-#     RETROPIE: 16,
-#     SPOTIFY: 32,
-#     WAP: 64,
-#     WRT: 128,
-#     CNC: 256,
-#     OBD: 512,
-#     GPS: 1024,
-#     SENSOR_TEMP: 2048,
-#     SENSOR_HUM: 4096,
-#     SENSOR_COLOR: 8192,
-#     SENSOR_LIGHT: 16384,
-#     CONTROLLER_STEPPER: 32768,
-#     CONTROLLER_DCMOTOR: 65536,
-#     CONTROLLER_SERVO: 131072,
-#     CAMERA: 262144
-# }
-
-# var PiModel = {
-#     M1A: 0,
-#     M1AP: 1,
-#     M1B: 2,
-#     M1BP: 3,
-#     M2B: 4,
-#     M2BV12: 5,
-#     M3AP: 6,
-#     M3B: 7,
-#     M3BP: 8,
-#     M4B: 9,
-#     CM1: 10,
-#     CM3: 11,
-#     CM3L: 12,
-#     CM3P: 13,
-#     CM3LP: 14,
-#     ZV12: 15,
-#     ZV13: 16,
-#     ZW: 17
-# }
--- scratch.py
+++ scratch.py
@@ -1,16 +1,25 @@
+import copy
+import json
 
-r = 16
-g = 0
-b = 0
-for i in range(48):
-    if g < 16 and b == 0:
-        r = r - 1
-        g = g + 1
-    elif b < 16 and r == 0:
-        g = g - 1
-        b = b + 1
-    else:
-        b = b - 1
-        r = r + 1
-    print((r,g,b))
-    
+def get_palette():
+    r = 16
+    g = 0
+    b = 0
+    palette = []
+    for i in range(48):
+        if g < 16 and b == 0:
+            r = r - 1
+            g = g + 1
+        elif b < 16 and r == 0:
+            g = g - 1
+            b = b + 1
+        else:
+            b = b - 1
+            r = r + 1
+        palette.append((r,g,b))
+    last = copy.copy(palette[len(palette)-1])
+    del palette[len(palette)-1]
+    palette.insert(0,last)
+    return palette
+palette = get_palette()
+print(json.dumps(palette,indent=4))
